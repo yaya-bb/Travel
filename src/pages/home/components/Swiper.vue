92843fd62f4994e1bb75a234aec8c42e28b782ac
yaya-bb <2286834433@qq.com>
2022-07-23 17:31:06 +0800

    首页轮播图

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="wrapper">
+      <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of list" :key="item.id">
+         <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
+  data() {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+        // 支持循环轮播
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff !important
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    // 始终保持宽高比需要设置height:0+overflow: hidden+padding-bottom:宽高比
+</style>
