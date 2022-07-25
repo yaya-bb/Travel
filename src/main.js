a0995dfa10ac5edca11e4fe02502704f1973881b
yaya-bb <2286834433@qq.com>
2022-07-25 20:05:08 +0800

    axios异步请求

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import fastClick from 'fastClick'
-import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
-import 'swiper/dist/css/swiper.css'
+import 'swiper/css/swiper.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 Vue.config.productionTip = false
 fastClick.attach(document.body)
 Vue.use(VueAwesomeSwiper)
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -13,6 +13,7 @@ import HomeSwiper from './components/Swiper.vue'
 import HomeIcons from './components/Icons.vue'
 import HomeRecommend from './components/Recommend.vue'
 import HomeWeekend from './components/Weekend.vue'
+import axios from 'axios'
 export default{
   name: 'Home',
   components: {
@@ -21,6 +22,18 @@ export default{
     HomeIcons,
     HomeRecommend,
     HomeWeekend
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json')
+        .then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      console.log(res)
+    }
+  },
+  mounted () {
+    this.getHomeInfo()
   }
 }
 </script>
