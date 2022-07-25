169d30243fc88b040ce2ee26b684357010872bfe
yaya-bb <2286834433@qq.com>
2022-07-25 23:13:05 +0800

    数据传递

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -8,14 +8,17 @@
       输入城市/景点/游玩城市
     </div>
     <div class="header-right">
-      城市
+      北京
       <span class="iconfont arrow-icon">&#xe600;</span>
     </div>
   </div>
 </template>
 <script>
 export default{
-  name: 'HomeHeader'
+  name: 'HomeHeader',
+  props: {
+    city: String
+  }
 }
 </script>
 
--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="icons">
+  <div class="icons" :options="swiperOption" v-if="showSwiper">
     <swiper>
       <swiper-slide v-for="(page, index) in pages" :key="index">
         <div 
@@ -20,59 +20,21 @@
 <script>
 export default{
   name: 'HomeIcons',
+  props: {
+    list: Array
+  },
   data () {
     return {
-      iconList: [{
-      id:"0001",
-      imgUrl:"http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
-      desc:'景点门票'
-      },
-      {
-        id:"0002",
-        imgUrl:"http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png",
-        desc:'滑雪季'
-      },
-      {
-        id:"0003",
-        imgUrl:"http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png",
-        desc:'动植园'
-      },
-      {
-        id:"0004",
-        imgUrl:"http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
-        desc:'景点门票'
-      },
-      {
-        id:"0005",
-        imgUrl:"http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png",
-        desc:'滑雪季'
-      },
-      {
-        id:"0006",
-        imgUrl:"http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png",
-        desc:'动植园'
-      },
-      {
-        id:"0007",
-        imgUrl:"http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
-        desc:'景点门票'
-      },
-      {
-        id:"0008",
-        imgUrl:"http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png",
-        desc:'滑雪季'
-      },
-      {
-        id:"0009",
-        imgUrl:"http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png",
-        desc:'动植园'
-      }]
+      swiperOption: {
+        swiperOption: '.swiper-pagination',
+        loop: true
+      }
     }
   },
   computed: {
     pages () {
       const pages = []
-      this.iconList.forEach((item, index) => {
+      this.list.forEach((item, index) => {
         const page = Math.floor(index/8)
         if(!pages[page]) {
           pages[page] = []
@@ -80,6 +42,9 @@ export default{
         pages[page].push(item)
       })
     return pages
+    },
+    showSwiper () {
+      return this.list.length
     }
   }
 }
